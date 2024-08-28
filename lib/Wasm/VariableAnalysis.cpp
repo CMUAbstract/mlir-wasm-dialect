907306879e0608132ec743bcd1a2dbf45d33dd09
Byeongjee Kang <byeongjee.kang@gmail.com>
2024-08-27 20:15:59 -0400

    Update VariableAnalysis pass to use temp local decls

--- lib/Wasm/VariableAnalysis.cpp
+++ lib/Wasm/VariableAnalysis.cpp
@@ -1,9 +1,10 @@
-#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 
 #include "Wasm/VariableAnalysis.h"
+#include "Wasm/WasmOps.h"
 
 using namespace mlir;
+using namespace mlir::wasm;
 
 namespace mlir::wasm {
 VariableAnalysis::VariableAnalysis(Operation *op) {
@@ -11,40 +12,36 @@ VariableAnalysis::VariableAnalysis(Operation *op) {
   numVariables = 0;
   if (auto func = dyn_cast<func::FuncOp>(op)) {
     numArguments = func.getNumArguments();
-    // TODO: initialize reg2Loc with arguments
 
     func.walk([&](Operation *op) {
-      if (auto constantOp = dyn_cast<arith::ConstantOp>(op)) {
+      if (auto constantOp = dyn_cast<TempLocalOp>(op)) {
         mlir::Value result = constantOp.getResult();
         reg2Loc.push_back(result);
         numVariables++;
       }
-      if (auto addOp = dyn_cast<arith::AddIOp>(op)) {
-        mlir::Value result = addOp.getResult();
-        reg2Loc.push_back(result);
-        numVariables++;
-      }
-      // TODO: handle other operations that define new variables
     });
   }
 }
 
 int VariableAnalysis::getNumVariables() { return numVariables; }
 
-int VariableAnalysis::getLocalIndex(const Value &reg) {
-  auto result = std::find(reg2Loc.begin(), reg2Loc.end(), reg);
+int VariableAnalysis::getLocalIndex(const Value &tempLocal) {
+  auto result = std::find(reg2Loc.begin(), reg2Loc.end(), tempLocal);
   if (result != reg2Loc.end()) {
     return result - reg2Loc.begin() + numArguments;
   }
+  // TODO: Error handling
   return -1;
 }
 
 vector<Attribute> VariableAnalysis::getTypeAttrs() {
   vector<Attribute> types;
   types.reserve(reg2Loc.size());
-  std::transform(
-      reg2Loc.begin(), reg2Loc.end(), std::back_inserter(types),
-      [](const auto &reg) { return mlir::TypeAttr::get(reg.getType()); });
+  std::transform(reg2Loc.begin(), reg2Loc.end(), std::back_inserter(types),
+                 [](const auto &tempLocal) {
+                   return mlir::TypeAttr::get(
+                       dyn_cast<LocalType>(tempLocal.getType()).getInner());
+                 });
   return types;
 }
 } // namespace mlir::wasm
