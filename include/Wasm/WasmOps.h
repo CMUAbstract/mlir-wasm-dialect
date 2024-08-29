2d5c6961152cdbf93b051feab1eed5f3beec7e1a
Byeongjee Kang <byeongjee.kang@gmail.com>
2024-08-29 12:43:01 -0400

    Add WasmReturnOp

--- include/Wasm/WasmOps.h
+++ include/Wasm/WasmOps.h
@@ -10,6 +10,7 @@
 #define WASM_WASMOPS_H
 
 #include "mlir/Bytecode/BytecodeOpInterface.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/IR/Dialect.h"
 #include "mlir/IR/OpDefinition.h"
