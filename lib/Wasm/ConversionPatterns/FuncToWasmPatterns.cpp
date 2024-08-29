33e543b8e0109395d5ad91ce80b449f5d34d6347
Byeongjee Kang <byeongjee.kang@gmail.com>
2024-08-29 12:50:11 -0400

    Add skeleton for lowering func

--- lib/Wasm/ConversionPatterns/FuncToWasmPatterns.cpp
+++ lib/Wasm/ConversionPatterns/FuncToWasmPatterns.cpp
@@ -5,6 +5,22 @@ namespace mlir::wasm {
 
 void populateFuncToWasmPatterns(TypeConverter &converter,
                                 RewritePatternSet &patterns) {
+  patterns.add<ConvertFunc, ConvertReturn>(context);
   // TODO
 }
+
+LogicalResult
+ConvertFunc::matchAndRewrite(func::FuncOp op, OpAdaptor adaptor,
+                             ConversionPatternRewriter &rewriter) const {
+  // TODO
+  return success();
+}
+
+LogicalResult
+ConvertReturn::matchAndRewrite(func::ReturnOp op, OpAdaptor adaptor,
+                               ConversionPatternRewriter &rewriter) const {
+  // TODO
+  return success();
+}
+
 } // namespace mlir::wasm
