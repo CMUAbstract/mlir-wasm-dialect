d01e9519a57ac65dc7dbcca389580e381c860d4b
Byeongjee Kang <byeongjee.kang@gmail.com>
2024-09-08 18:11:32 -0400

    Fix compilation error

--- lib/Wasm/ConversionPatterns/FuncToWasmPatterns.cpp
+++ lib/Wasm/ConversionPatterns/FuncToWasmPatterns.cpp
@@ -5,65 +5,70 @@
 
 namespace mlir::wasm {
 
+struct FuncOpLowering : public OpConversionPattern<func::FuncOp> {
+  using OpConversionPattern<func::FuncOp>::OpConversionPattern;
+  LogicalResult
+  matchAndRewrite(func::FuncOp funcOp, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+
+    TypeConverter::SignatureConversion signatureConverter(
+        funcOp.getFunctionType().getNumInputs());
+    for (const auto &inputType :
+         enumerate(funcOp.getFunctionType().getInputs())) {
+      signatureConverter.addInputs(
+          inputType.index(),
+          LocalType::get(funcOp.getContext(), inputType.value()));
+    }
+
+    auto newFuncType =
+        rewriter.getFunctionType(signatureConverter.getConvertedTypes(),
+                                 funcOp.getFunctionType().getResults());
+    auto newFuncOp = rewriter.create<WasmFuncOp>(funcOp.getLoc(),
+                                                 funcOp.getName(), newFuncType);
+
+    if (failed(rewriter.convertRegionTypes(
+            &funcOp.getBody(), *getTypeConverter(), &signatureConverter))) {
+      return failure();
+    }
+
+    rewriter.inlineRegionBefore(funcOp.getBody(), newFuncOp.getBody(),
+                                newFuncOp.end());
+
+    for (const auto &namedAttr : funcOp->getAttrs()) {
+      if (namedAttr.getName() != funcOp.getFunctionTypeAttrName() &&
+          namedAttr.getName() != SymbolTable::getSymbolAttrName())
+        newFuncOp->setAttr(namedAttr.getName(), namedAttr.getValue());
+    }
+
+    rewriter.eraseOp(funcOp);
+
+    return success();
+  }
+};
+
+struct ReturnOpLowering : public OpConversionPattern<func::ReturnOp> {
+  using OpConversionPattern<func::ReturnOp>::OpConversionPattern;
+  LogicalResult
+  matchAndRewrite(func::ReturnOp returnOp, OpAdaptor adaptor,
+                  ConversionPatternRewriter &rewriter) const override {
+    for (auto operand : returnOp->getOperands()) {
+      // TODO: ideally TypeConverter should automatically handle type conversion
+      // but it doesn't seem to work because TempLocalGetOp are not the
+      // operations that are being converted
+      auto casted = typeConverter->materializeTargetConversion(
+          rewriter, returnOp.getLoc(),
+          typeConverter->convertType(operand.getType()), operand);
+      rewriter.create<TempLocalGetOp>(returnOp.getLoc(), casted);
+    }
+    rewriter.replaceOpWithNewOp<WasmReturnOp>(returnOp);
+
+    return success();
+  }
+};
+
 void populateFuncToWasmPatterns(TypeConverter &typeConverter,
                                 RewritePatternSet &patterns) {
   patterns.add<FuncOpLowering, ReturnOpLowering>(typeConverter,
                                                  patterns.getContext());
 }
-
-LogicalResult
-FuncOpLowering::matchAndRewrite(func::FuncOp funcOp, OpAdaptor adaptor,
-                                ConversionPatternRewriter &rewriter) const {
-
-  TypeConverter::SignatureConversion signatureConverter(
-      funcOp.getFunctionType().getNumInputs());
-  for (const auto &inputType :
-       enumerate(funcOp.getFunctionType().getInputs())) {
-    signatureConverter.addInputs(
-        inputType.index(),
-        LocalType::get(funcOp.getContext(), inputType.value()));
-  }
-
-  auto newFuncType =
-      rewriter.getFunctionType(signatureConverter.getConvertedTypes(),
-                               funcOp.getFunctionType().getResults());
-  auto newFuncOp = rewriter.create<WasmFuncOp>(funcOp.getLoc(),
-                                               funcOp.getName(), newFuncType);
-
-  if (failed(rewriter.convertRegionTypes(&funcOp.getBody(), *getTypeConverter(),
-                                         &signatureConverter))) {
-    return failure();
-  }
-
-  rewriter.inlineRegionBefore(funcOp.getBody(), newFuncOp.getBody(),
-                              newFuncOp.end());
-
-  for (const auto &namedAttr : funcOp->getAttrs()) {
-    if (namedAttr.getName() != funcOp.getFunctionTypeAttrName() &&
-        namedAttr.getName() != SymbolTable::getSymbolAttrName())
-      newFuncOp->setAttr(namedAttr.getName(), namedAttr.getValue());
-  }
-
-  rewriter.eraseOp(funcOp);
-
-  return success();
-}
-
-LogicalResult
-ReturnOpLowering::matchAndRewrite(func::ReturnOp returnOp, OpAdaptor adaptor,
-                                  ConversionPatternRewriter &rewriter) const {
-  for (auto operand : returnOp->getOperands()) {
-    // TODO: ideally TypeConverter should automatically handle type conversion
-    // but it doesn't seem to work because TempLocalGetOp are not the operations
-    // that are being converted
-    auto casted = typeConverter->materializeTargetConversion(
-        rewriter, returnOp.getLoc(),
-        typeConverter->convertType(operand.getType()), operand);
-    rewriter.create<TempLocalGetOp>(returnOp.getLoc(), casted);
-  }
-  rewriter.replaceOpWithNewOp<WasmReturnOp>(returnOp);
-
-  return success();
-}
-
 } // namespace mlir::wasm
