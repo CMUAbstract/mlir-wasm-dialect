e4f38b6ff2e19313ccb045b755f0b19d4e44d27f
Byeongjee Kang <byeongjee.kang@gmail.com>
2024-09-09 22:42:29 -0400

    Add missing dependencies

--- wasm-opt/wasm-opt.cpp
+++ wasm-opt/wasm-opt.cpp
@@ -22,7 +22,8 @@ int main(int argc, char **argv) {
 
   mlir::DialectRegistry registry;
   registry.insert<mlir::wasm::WasmDialect, mlir::arith::ArithDialect,
-                  mlir::func::FuncDialect, mlir::scf::SCFDialect>();
+                  mlir::func::FuncDialect, mlir::scf::SCFDialect,
+                  mlir::memref::MemRefDialect>();
   // Add the following to include *all* MLIR Core dialects, or selectively
   // include what you need like above. You only need to register dialects that
   // will be *parsed* by the tool, not the one generated
