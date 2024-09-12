651cdff268d39128bdedbb57eb6fec07f5b94c2e
Byeongjee Kang <byeongjee.kang@gmail.com>
2024-09-11 21:49:16 -0400

    Implement utility function for data formatting

--- lib/Wasm/ConversionPatterns/MemRefToWasmPatterns.cpp
+++ lib/Wasm/ConversionPatterns/MemRefToWasmPatterns.cpp
@@ -1,6 +1,44 @@
 #include "Wasm/ConversionPatterns/MemRefToWasmPatterns.h"
 #include "Wasm/WasmOps.h"
 
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+static const uint8_t s_is_char_escaped[] = {
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+static const char s_hexdigits[] = "0123456789abcdef";
+
+std::string generateStr(const void *bytes, size_t length) {
+  const uint8_t *u8_data = static_cast<const uint8_t *>(bytes);
+
+  std::stringstream ss;
+
+  for (size_t i = 0; i < length; ++i) {
+    uint8_t c = u8_data[i];
+    if (s_is_char_escaped[c]) {
+      ss << "\\";
+      ss << s_hexdigits[c >> 4];
+      ss << s_hexdigits[c & 0xf];
+    } else {
+      ss << c;
+    }
+  }
+
+  return ss.str();
+}
+
 namespace mlir::wasm {
 
 struct GlobalOpLowering : public OpConversionPattern<memref::GlobalOp> {
