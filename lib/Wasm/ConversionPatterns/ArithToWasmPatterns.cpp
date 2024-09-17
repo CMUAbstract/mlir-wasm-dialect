0ff0b1966c12d071b9c0e32932126d405682fa0d
Byeongjee Kang <byeongjee.kang@gmail.com>
2024-09-17 15:44:58 -0400

    Refactor ArithToWasmPatterns.cpp

--- lib/Wasm/ConversionPatterns/ArithToWasmPatterns.cpp
+++ lib/Wasm/ConversionPatterns/ArithToWasmPatterns.cpp
@@ -4,38 +4,47 @@
 
 namespace mlir::wasm {
 
+template <typename S, typename T>
+LogicalResult matchAndRewriteBinaryOp(S op, ConversionPatternRewriter &rewriter,
+                                      const TypeConverter *typeConverter) {
+  Location loc = op->getLoc();
+  Value result = op.getResult();
+  Type type = op.getType();
+
+  auto tempLocalOp = rewriter.create<wasm::TempLocalOp>(loc, type);
+
+  auto lhs = op.getOperand(0);
+  auto rhs = op.getOperand(1);
+
+  if ((lhs.getType() != type) || (rhs.getType() != type)) {
+    return rewriter.notifyMatchFailure(op, "type mismatch");
+  }
+
+  auto localType = typeConverter->convertType(type);
+  auto castedLhs =
+      typeConverter->materializeTargetConversion(rewriter, loc, localType, lhs);
+  auto castedRhs =
+      typeConverter->materializeTargetConversion(rewriter, loc, localType, rhs);
+
+  rewriter.create<wasm::TempLocalGetOp>(loc, castedLhs);
+  rewriter.create<wasm::TempLocalGetOp>(loc, castedRhs);
+
+  rewriter.create<T>(loc, type);
+
+  rewriter.create<wasm::TempLocalSetOp>(loc, tempLocalOp.getResult());
+  rewriter.replaceOp(op, tempLocalOp);
+
+  return success();
+}
+
 struct MulFOpLowering : public OpConversionPattern<arith::MulFOp> {
   using OpConversionPattern<arith::MulFOp>::OpConversionPattern; // Constructor
 
   LogicalResult
   matchAndRewrite(arith::MulFOp mulFOp, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
-
-    Location loc = mulFOp->getLoc();
-    Value result = mulFOp.getResult();
-    Type type = mulFOp.getType();
-
-    auto tempLocalOp = rewriter.create<wasm::TempLocalOp>(loc, type);
-
-    if ((mulFOp.getLhs().getType() != type) ||
-        (mulFOp.getRhs().getType() != type)) {
-      return rewriter.notifyMatchFailure(mulFOp, "type mismatch");
-    }
-
-    auto localType = typeConverter->convertType(type);
-    auto castedLhs = typeConverter->materializeTargetConversion(
-        rewriter, loc, localType, mulFOp.getLhs());
-    auto castedRhs = typeConverter->materializeTargetConversion(
-        rewriter, loc, localType, mulFOp.getRhs());
-
-    rewriter.create<wasm::TempLocalGetOp>(loc, castedLhs);
-    rewriter.create<wasm::TempLocalGetOp>(loc, castedRhs);
-
-    rewriter.create<wasm::MulOp>(loc, type);
-    rewriter.create<wasm::TempLocalSetOp>(loc, tempLocalOp.getResult());
-    rewriter.replaceOp(mulFOp, tempLocalOp);
-
-    return success();
+    return matchAndRewriteBinaryOp<arith::MulFOp, wasm::MulOp>(mulFOp, rewriter,
+                                                               typeConverter);
   }
 };
 
@@ -44,31 +53,9 @@ struct AddFOpLowering : public OpConversionPattern<arith::AddFOp> {
   LogicalResult
   matchAndRewrite(arith::AddFOp addFOp, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
-    Location loc = addFOp->getLoc();
-    Value result = addFOp.getResult();
-    Type type = addFOp.getType();
 
-    auto tempLocalOp = rewriter.create<wasm::TempLocalOp>(loc, type);
-
-    if ((addFOp.getLhs().getType() != type) ||
-        (addFOp.getRhs().getType() != type)) {
-      return rewriter.notifyMatchFailure(addFOp, "type mismatch");
-    }
-
-    auto localType = typeConverter->convertType(type);
-    auto castedLhs = typeConverter->materializeTargetConversion(
-        rewriter, loc, localType, addFOp.getLhs());
-    auto castedRhs = typeConverter->materializeTargetConversion(
-        rewriter, loc, localType, addFOp.getRhs());
-
-    rewriter.create<wasm::TempLocalGetOp>(loc, castedLhs);
-    rewriter.create<wasm::TempLocalGetOp>(loc, castedRhs);
-
-    rewriter.create<wasm::AddOp>(loc, type);
-    rewriter.create<wasm::TempLocalSetOp>(loc, tempLocalOp.getResult());
-    rewriter.replaceOp(addFOp, tempLocalOp);
-
-    return success();
+    return matchAndRewriteBinaryOp<arith::AddFOp, wasm::AddOp>(addFOp, rewriter,
+                                                               typeConverter);
   }
 };
 
@@ -77,33 +64,9 @@ struct AddIOpLowering : public OpConversionPattern<arith::AddIOp> {
   LogicalResult
   matchAndRewrite(arith::AddIOp addIOp, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
-    Location loc = addIOp->getLoc();
-    Value result = addIOp.getResult();
-    Type type = result.getType();
 
-    auto tempLocalOp = rewriter.create<wasm::TempLocalOp>(loc, type);
-
-    if ((addIOp.getLhs().getType() != type) ||
-        (addIOp.getRhs().getType() != type)) {
-      return rewriter.notifyMatchFailure(addIOp, "type mismatch");
-    }
-    // TODO: ideally TypeConverter should automatically handle type conversion
-    // but it doesn't seem to work because TempLocalGetOp are not the operations
-    // that are being converted
-    auto localType = typeConverter->convertType(type);
-    auto castedLhs = typeConverter->materializeTargetConversion(
-        rewriter, loc, localType, addIOp.getLhs());
-    auto castedRhs = typeConverter->materializeTargetConversion(
-        rewriter, loc, localType, addIOp.getRhs());
-
-    rewriter.create<wasm::TempLocalGetOp>(loc, castedLhs);
-    rewriter.create<wasm::TempLocalGetOp>(loc, castedRhs);
-
-    rewriter.create<wasm::AddOp>(loc, type);
-    rewriter.create<wasm::TempLocalSetOp>(loc, tempLocalOp.getResult());
-    rewriter.replaceOp(addIOp, tempLocalOp);
-
-    return success();
+    return matchAndRewriteBinaryOp<arith::AddIOp, wasm::AddOp>(addIOp, rewriter,
+                                                               typeConverter);
   }
 };
 
