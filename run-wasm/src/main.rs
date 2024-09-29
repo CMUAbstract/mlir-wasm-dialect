40db3d659f0386c1bcc19eaaad5747436af78341
Byeongjee Kang <byeongjee.kang@gmail.com>
2024-09-28 20:41:33 -0400

    Prepare for execution

--- run-wasm/src/main.rs
+++ run-wasm/src/main.rs
@@ -1,4 +1,4 @@
-use aligned_array::{Aligned, A32};
+use aligned_array::{Aligned, A32, A8};
 use bytemuck;
 use std::time::Instant;
 use wasmtime::{Engine, Instance, Module, Result, Store};
@@ -6,12 +6,16 @@ use wasmtime::{Engine, Instance, Module, Result, Store};
 mod mnist;
 
 fn main() -> Result<()> {
-    // Currently, we use WASI because enabling WASI is the easiest way to link
-    // libc functions (malloc and free) at compile time. However, we need to
-    // eliminate the WASI dependency in the future since we are not using any
-    // WASI functions.
+    let is_my_compiler = true;
     let engine = Engine::default();
-    let module = Module::from_file(&engine, "../test/conv2d-linked.wat")?;
+
+    let src = if is_my_compiler {
+        "../test/conv2d-out-linked.wat"
+    } else {
+        "../test/conv2d-out-linked-mlir.wat"
+    };
+
+    let module = Module::from_file(&engine, src)?;
     let mut store = Store::new(&engine, ());
     let instance = Instance::new(&mut store, &module, &[])?;
     let memory = instance
@@ -23,11 +27,6 @@ fn main() -> Result<()> {
         .expect("malloc not found")
         .typed::<i32, i32>(&store)?;
 
-    let main_fn = instance
-        .get_func(&mut store, "_mlir_ciface_main")
-        .expect("_mlir_ciface_main not found")
-        .typed::<(i32, i32), ()>(&store)?;
-
     let scaled_data: Vec<_> = mnist::SAMPLE_DATA
         .into_iter()
         .map(|x| (x as f32) / 255.0)
@@ -38,68 +37,101 @@ fn main() -> Result<()> {
         [tensor_ptr as usize..tensor_ptr as usize + mnist::INPUT_TENSOR_SIZE]
         .copy_from_slice(bytemuck::cast_slice(&scaled_data));
 
-    let input_ptr = malloc_fn.call(&mut store, mnist::INPUT_SIZE as i32)?;
-    let input = mnist::Input {
-        base_ptr: tensor_ptr,
-        data: tensor_ptr,
-        offset: 0,
-        sizes: [1, 28, 28],
-        strides: [28 * 28, 28, 1],
-    };
-    memory.data_mut(&mut store)[input_ptr as usize..input_ptr as usize + mnist::INPUT_SIZE]
-        .copy_from_slice(bytemuck::bytes_of(&input));
+    if is_my_compiler {
+        let main_fn = instance
+            .get_func(&mut store, "main")
+            .expect("main not found")
+            .typed::<i32, i32>(&store)?;
 
-    let output_tensor_ptr = malloc_fn.call(&mut store, mnist::OUTPUT_TENSOR_SIZE as i32)?;
-    let output_ptr = malloc_fn.call(&mut store, mnist::OUTPUT_SIZE as i32)?;
-    let output = mnist::Output {
-        base_ptr: output_tensor_ptr,
-        data: output_tensor_ptr,
-        offset: 0,
-        sizes: [1, 10],
-        strides: [10, 1],
-    };
-    memory.data_mut(&mut store)[output_ptr as usize..output_ptr as usize + mnist::OUTPUT_SIZE]
-        .copy_from_slice(bytemuck::bytes_of(&output));
+        let now = Instant::now();
+        let output_ptr = main_fn.call(&mut store, tensor_ptr)?;
+        println!("output_ptr: {:?}\n", output_ptr);
+        let elapsed = now.elapsed();
+        println!("Elapsed: {:.2?}", elapsed);
 
-    let now = Instant::now();
-    main_fn.call(&mut store, (output_ptr, input_ptr))?;
-    let elapsed = now.elapsed();
-    println!("Elapsed: {:.2?}", elapsed);
+        let mut output_tensor_buffer: Aligned<A8, [u8; 40]> = Aligned([0u8; 40]);
+        memory.read(
+            &store,
+            output_ptr as usize,
+            output_tensor_buffer.as_mut_slice(),
+        )?;
 
-    let mut output_buffer = [0u8; mnist::OUTPUT_SIZE];
-    memory.read(&store, output_ptr as usize, &mut output_buffer)?;
+        for (i, score) in bytemuck::cast_slice::<u8, f32>(output_tensor_buffer.as_slice())
+            .iter()
+            .enumerate()
+        {
+            println!("{}: {}", i, score);
+        }
+    } else {
+        let main_fn = instance
+            .get_func(&mut store, "_mlir_ciface_main")
+            .expect("_mlir_ciface_main not found")
+            .typed::<(i32, i32), ()>(&store)?;
 
-    let output: mnist::Output = bytemuck::cast(output_buffer);
+        let input_ptr = malloc_fn.call(&mut store, mnist::INPUT_SIZE as i32)?;
+        let input = mnist::Input {
+            base_ptr: tensor_ptr,
+            data: tensor_ptr,
+            offset: 0,
+            sizes: [1, 28, 28],
+            strides: [28 * 28, 28, 1],
+        };
+        memory.data_mut(&mut store)[input_ptr as usize..input_ptr as usize + mnist::INPUT_SIZE]
+            .copy_from_slice(bytemuck::bytes_of(&input));
 
-    // We need aligned array because
-    // we are converting byte array to f32 array
-    //
-    let mut output_tensor_buffer: Aligned<A32, [u8; 40]> =
-        Aligned([0u8; mnist::OUTPUT_TENSOR_SIZE]);
-    memory.read(
-        &store,
-        output.data as usize,
-        output_tensor_buffer.as_mut_slice(),
-    )?;
+        let output_tensor_ptr = malloc_fn.call(&mut store, mnist::OUTPUT_TENSOR_SIZE as i32)?;
+        let output_ptr = malloc_fn.call(&mut store, mnist::OUTPUT_SIZE as i32)?;
+        let output = mnist::Output {
+            base_ptr: output_tensor_ptr,
+            data: output_tensor_ptr,
+            offset: 0,
+            sizes: [1, 10],
+            strides: [10, 1],
+        };
+        memory.data_mut(&mut store)[output_ptr as usize..output_ptr as usize + mnist::OUTPUT_SIZE]
+            .copy_from_slice(bytemuck::bytes_of(&output));
 
-    for (i, score) in bytemuck::cast_slice::<u8, f32>(output_tensor_buffer.as_slice())
-        .iter()
-        .enumerate()
-    {
-        println!("{}: {}", i, score);
+        let now = Instant::now();
+        main_fn.call(&mut store, (output_ptr, input_ptr))?;
+        let elapsed = now.elapsed();
+        println!("Elapsed: {:.2?}", elapsed);
+
+        let mut output_buffer = [0u8; mnist::OUTPUT_SIZE];
+        memory.read(&store, output_ptr as usize, &mut output_buffer)?;
+
+        let output: mnist::Output = bytemuck::cast(output_buffer);
+
+        // We need aligned array because
+        // we are converting byte array to f32 array
+        //
+        let mut output_tensor_buffer: Aligned<A32, [u8; 40]> =
+            Aligned([0u8; mnist::OUTPUT_TENSOR_SIZE]);
+        memory.read(
+            &store,
+            output.data as usize,
+            output_tensor_buffer.as_mut_slice(),
+        )?;
+
+        for (i, score) in bytemuck::cast_slice::<u8, f32>(output_tensor_buffer.as_slice())
+            .iter()
+            .enumerate()
+        {
+            println!("{}: {}", i, score);
+        }
+        // expected output
+
+        //    Elapsed: 28.46µs
+        //    0: 8.469532
+        //    1: 0
+        //    2: 0
+        //    3: 0
+        //    4: 0
+        //    5: 0
+        //    6: 0
+        //    7: 0
+        //    8: 0
+        //    9: 0
     }
-    // expected output
 
-    //    Elapsed: 28.46µs
-    //    0: 8.469532
-    //    1: 0
-    //    2: 0
-    //    3: 0
-    //    4: 0
-    //    5: 0
-    //    6: 0
-    //    7: 0
-    //    8: 0
-    //    9: 0
     Ok(())
 }
