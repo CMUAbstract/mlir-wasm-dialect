e548c6da2ca4f87284e78d401d73097621f52567
Byeongjee Kang <byeongjee.kang@gmail.com>
2024-10-14 18:09:55 -0400

    Support running wasm as well as aot

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ ZEPHYRPROJECT="/Users/byeongje/zephyrproject/zephyr"
 
 # Check if the minimum number of arguments is provided
 if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 <mlir_file> --type=[mlir|llvm] [--optimize] -- <aot_flags>"
+    echo "Usage: $0 <mlir_file> --type=[mlir|llvm] [--optimize] [--use-aot=<true|false>] -- <aot_flags>"
     exit 1
 fi
 
@@ -13,6 +13,7 @@ fi
 MLIR_FILE=""
 COMPILATION_TYPE="mlir"  # Default type is mlir
 OPTIMIZE_FLAG=false
+USE_AOT=true  # Default is to use AOT
 AOT_FLAGS=""
 EXECUTION_TYPE=0
 
@@ -25,6 +26,10 @@ while [[ "$#" -gt 0 ]]; do
             ;;
         --optimize)
             OPTIMIZE_FLAG=true
+            shift
+            ;;
+        --use-aot=*)
+            USE_AOT="${1#*=}"
             shift
             ;;
         --)
@@ -48,12 +53,6 @@ fi
 # Check if MLIR file is provided
 if [ -z "$MLIR_FILE" ]; then
     echo "Error: MLIR or LLVM file not provided."
-    exit 1
-fi
-
-# Check if AOT flags were provided
-if [ -z "$AOT_FLAGS" ]; then
-    echo "Error: AOT compilation flags not provided."
     exit 1
 fi
 
@@ -86,38 +85,54 @@ if [ ! -f "$TEMP_DIR/$BASENAME.wasm" ]; then
     exit 1
 fi
 
-# Step 3: Compile Wasm to AOT
-AOT_COMPILE_CMD="./mcu-wasm-executor/aot-compiler/compile_aot.sh -i $TEMP_DIR/$BASENAME.wasm -o $TEMP_DIR/$BASENAME.aot -- $AOT_FLAGS"
+# Step 3: Conditionally compile Wasm to AOT based on --use-aot
+if [ "$USE_AOT" = true ]; then
+    AOT_COMPILE_CMD="./mcu-wasm-executor/aot-compiler/compile_aot.sh -i $TEMP_DIR/$BASENAME.wasm -o $TEMP_DIR/$BASENAME.aot -- $AOT_FLAGS"
 
-echo "Compiling Wasm to AOT with command: $AOT_COMPILE_CMD"
-eval "$AOT_COMPILE_CMD"
+    echo "Compiling Wasm to AOT with command: $AOT_COMPILE_CMD"
+    eval "$AOT_COMPILE_CMD"
 
-# Check if the AOT file was created successfully
-if [ ! -f "$TEMP_DIR/$BASENAME.aot" ]; then
-    echo "Error: AOT file not found in $TEMP_DIR"
-    exit 1
+    # Check if the AOT file was created successfully
+    if [ ! -f "$TEMP_DIR/$BASENAME.aot" ]; then
+        echo "Error: AOT file not found in $TEMP_DIR"
+        exit 1
+    fi
+
+    # Set the file to be used for execution (AOT)
+    EXEC_FILE="$TEMP_DIR/$BASENAME.aot"
+else
+    # Set the file to be used for execution (Wasm)
+    EXEC_FILE="$TEMP_DIR/$BASENAME.wasm"
 fi
 
-# Step 4: Run AOT on device
-echo "Running AOT on device..."
-
-# Activate the virtual environment for Zephyr
-source $ZEPHYRPROJECT/.venv/bin/activate
-
+# Set the execution type for LLVM or MLIR
 if [ "$COMPILATION_TYPE" = "llvm" ]; then
     EXECUTION_TYPE=1
 fi
 
-# Move to the MCU Wasm Executor directory and prepare to run the AOT binary
-(cd mcu-wasm-executor && \
-    xxd -i -n wasm_file "../$TEMP_DIR/$BASENAME.aot" src/wasm.h && \
-    west build . -b apollo4p_blue_kxr_evb -p -- -DEXECUTION_TYPE=$EXECUTION_TYPE && \
-    west flash)
+# Step 4: Function to run the compiled file on the device
+run_on_device() {
+    local file=$1
 
-# Check if the build and flash were successful
-if [ $? -eq 0 ]; then
-    echo "AOT binary successfully flashed and executed on the device."
-else
-    echo "Error: Failed to build or flash the AOT binary."
-    exit 1
-fi
+    echo "Running on device using $file..."
+
+    # Activate the virtual environment for Zephyr
+    source $ZEPHYRPROJECT/.venv/bin/activate
+
+    # Move to the MCU Wasm Executor directory and prepare to run the binary
+    (cd mcu-wasm-executor && \
+        xxd -i -n wasm_file "../$file" src/wasm.h && \
+        west build . -b apollo4p_blue_kxr_evb -p -- -DEXECUTION_TYPE=$EXECUTION_TYPE && \
+        west flash)
+
+    # Check if the build and flash were successful
+    if [ $? -eq 0 ]; then
+        echo "Binary successfully flashed and executed on the device."
+    else
+        echo "Error: Failed to build or flash the binary."
+        exit 1
+    fi
+}
+
+# Step 5: Run the compiled file (either AOT or Wasm) on the device
+run_on_device "$EXEC_FILE"
