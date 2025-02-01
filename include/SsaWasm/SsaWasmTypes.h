25b7382e8e01fe829cc7933c4e90271641e1571c
Byeongjee Kang <byeongjee.kang@gmail.com>
2025-02-01 14:42:47 -0500

    Add skeleton files for SsaWasm dialect

---
+++ include/SsaWasm/SsaWasmTypes.h
@@ -0,0 +1,17 @@
+//===- SsaWasmTypes.h - SsaWasm dialect types -------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SSAWASM_SSAWASMTYPES_H
+#define SSAWASM_SSAWASMTYPES_H
+
+#include "mlir/IR/BuiltinTypes.h"
+
+#define GET_TYPEDEF_CLASSES
+#include "SsaWasm/SsaWasmTypes.h.inc"
+
+#endif // SSAWASM_SSAWASMTYPES_H
