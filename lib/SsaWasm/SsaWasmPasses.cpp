76cfc895ad9dae6a6272c3f1924e5a2e4b72f976
Byeongjee Kang <byeongjee.kang@gmail.com>
2025-02-02 20:39:52 -0500

    Add skeleton for ssawasm passes

--- lib/SsaWasm/SsaWasmPasses.cpp
+++ lib/SsaWasm/SsaWasmPasses.cpp
@@ -14,6 +14,9 @@
 #include "SsaWasm/ConversionPatterns/FuncToSsaWasm.h"
 #include "SsaWasm/SsaWasmPasses.h"
 #include "SsaWasm/SsaWasmTypeConverter.h"
+#include <vector>
+
+using namespace std;
 
 namespace mlir::ssawasm {
 #define GEN_PASS_DEF_CONVERTTOSSAWASM
@@ -43,4 +46,110 @@ public:
   }
 };
 
+class UseCount {
+public:
+  UseCount(ModuleOp module) {
+    module.walk([&](FuncOp funcOp) {
+      for (auto &block : funcOp.getBody().getBlocks()) {
+        for (auto &op : block) {
+          useCount[&op] = op.getUsers().size();
+        }
+      }
+    });
+  }
+  int getUseCount(Operation *op) const { return useCount.at(op); }
+
+private:
+  DenseMap<Operation *, int> useCount;
+};
+
+namespace {}
+
+class ReplaceMultiUseOpsWithLocals
+    : public impl::ReplaceMultiUseOpsWithLocalsBase<
+          ReplaceMultiUseOpsWithLocals> {
+public:
+  using impl::ReplaceMultiUseOpsWithLocalsBase<
+      ReplaceMultiUseOpsWithLocals>::ReplaceMultiUseOpsWithLocalsBase;
+
+  void runOnOperation() final {
+    auto module = getOperation();
+    MLIRContext *context = module.getContext();
+    OpBuilder builder(context);
+
+    UseCount useCount(module);
+    // TODO
+  }
+};
+
+class ConvertSsaWasmToWasm
+    : public impl::ConvertSsaWasmToWasmBase<ConvertSsaWasmToWasm> {
+public:
+  using impl::ConvertSsaWasmToWasmBase<
+      ConvertSsaWasmToWasm>::ConvertSsaWasmToWasmBase;
+
+  void runOnOperation() final {
+    auto module = getOperation();
+    MLIRContext *context = module.getContext();
+    OpBuilder builder(context);
+
+    UseCount useCount(module);
+
+    module.walk([&](FuncOp funcOp) {
+      for (auto &block : funcOp.getBody().getBlocks()) {
+        stackifyBlock(builder, &block);
+      }
+    });
+    // TODO: Convert SsaWasm loop ops to Wasm loop ops
+    // TODO: Convert ssawasm::Func to wasm::WasmFunc
+  }
+
+private:
+  void stackifyBlock(OpBuilder &builder, Block *block, UseCount &useCount) {
+    vector<Operation *> ops;
+    for (auto &op : *block) {
+      ops.push_back(&op);
+    }
+
+    int index = ops.size() - 1;
+
+    while (index >= 0) {
+      recoverTree(builder, index, ops, useCount);
+    }
+  }
+
+  void recoverTree(OpBuilder &builder, int &index, vector<Operation *> &ops,
+                   UseCount &useCount) {
+    Operation *currentOp = ops[index];
+    SmallVector<Value, 4> nestedOperands;
+
+    ops.pop_back();
+    for (int operandIdx = currentOp->getNumOperands() - 1; operandIdx >= 0;
+         --operandIdx) {
+      Value operand = currentOp->getOperand(operandIdx);
+      Operation *definingOp = operand.getDefiningOp();
+      if (!definingOp) {
+        assert(isa<BlockArgument>(operand) && "Expected a block argument");
+        // Block argument can be used directly.
+        nestedOperands.insert(nestedOperands.begin(), operand);
+      } else if (useCount.getUseCount(definingOp) == 1 && index > 0 &&
+                 ops[index - 1] == definingOp) {
+        // This is a value defined by an operation that is used only once
+        // and the operation is the previous operation in the block.
+        index--;
+        Operation *nestedOp = recoverTree(builder, index, ops, useCount);
+        nestedOperands.insert(nestedOperands.begin(), nestedOp->getResult(0));
+      } else {
+        // TODO
+      }
+    }
+
+    stackifyOperation(builder, currentOp, nestedOperands);
+    // TODO
+  }
+  void stackifyOperation(OpBuilder &builder, Operation *op,
+                         SmallVector<Value, 4> &operands) {
+    // TODO
+  }
+};
 } // namespace mlir::ssawasm
