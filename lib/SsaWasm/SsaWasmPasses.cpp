11911858103893ba9cbbdef9f89935a66434b39a
Byeongjee Kang <byeongjee.kang@gmail.com>
2025-02-04 01:00:49 -0500

    Implement stackify pass except function conversion

--- lib/SsaWasm/SsaWasmPasses.cpp
+++ lib/SsaWasm/SsaWasmPasses.cpp
@@ -15,6 +15,7 @@
 #include "SsaWasm/ConversionPatterns/FuncToSsaWasm.h"
 #include "SsaWasm/SsaWasmPasses.h"
 #include "SsaWasm/SsaWasmTypeConverter.h"
+#include "Wasm/WasmOps.h"
 #include <vector>
 
 using namespace std;
@@ -196,13 +197,150 @@ public:
   }
 };
 
+class LocalIndexAnalysis {
+public:
+  LocalIndexAnalysis(ModuleOp module) {
+    module.walk([&](FuncOp funcOp) {
+      int index = 0;
+      // each wasm function argument is treated as a local
+      for (auto &arg : funcOp.getArguments()) {
+        localIndex[funcOp][arg] = index;
+        index++;
+      }
+
+      for (auto &block : funcOp.getBody().getBlocks()) {
+        for (auto &op : block) {
+          if (isa<LocalOp>(op)) {
+            localIndex[funcOp][op.getResult(0)] = index;
+            index++;
+          }
+        }
+      }
+    });
+  }
+  int getLocalIndex(FuncOp funcOp, Value value) const {
+    return localIndex.at(funcOp).at(value);
+  }
+
+private:
+  DenseMap<FuncOp, DenseMap<Value, int>> localIndex;
+};
+
 class Stackify : public impl::StackifyBase<Stackify> {
 public:
   using impl::StackifyBase<Stackify>::StackifyBase;
 
   void runOnOperation() final {
     auto module = getOperation();
-    // TODO
+    UseCountAnalysis useCount(module);
+    LocalIndexAnalysis localIndex(module);
+    OpBuilder builder(module.getContext());
+
+    module.walk([&](FuncOp funcOp) {
+      llvm::dbgs() << "Stackifying function\n";
+      funcOp.dump();
+      for (auto &block : funcOp.getBody().getBlocks()) {
+        llvm::dbgs() << "Stackifying block\n";
+        block.dump();
+        stackifyBlock(funcOp, &block, useCount, localIndex, builder);
+      }
+      // TODO: convert funcop and add local declarations
+    });
+  }
+
+private:
+  void stackifyBlock(FuncOp funcOp, Block *block,
+                     UseCountAnalysis &useCountAnalysis,
+                     LocalIndexAnalysis &localIndexAnalysis,
+                     OpBuilder &builder) {
+    vector<Operation *> ops;
+    for (auto &op : *block) {
+      ops.push_back(&op);
+    }
+
+    int index = ops.size() - 1;
+
+    while (index >= 0) {
+      stackify(funcOp, index, ops, useCountAnalysis, localIndexAnalysis,
+               builder);
+    }
+  }
+
+  // This modifies index in place
+  Operation *stackify(FuncOp funcOp, int &index, vector<Operation *> ops,
+                      UseCountAnalysis &useCountAnalysis,
+                      LocalIndexAnalysis &localIndexAnalysis,
+                      OpBuilder &builder) {
+    Operation *currentOp = ops[index];
+    llvm::dbgs() << "Stackifying operation\n";
+    currentOp->dump();
+    index--;
+
+    Operation *newOp =
+        stackifyOperation(currentOp, localIndexAnalysis, builder);
+    builder.setInsertionPoint(newOp);
+
+    if (isa<LocalOp>(currentOp)) {
+      currentOp->erase();
+      return newOp;
+    }
+
+    SmallVector<Value> operands;
+    for (int operandIdx = currentOp->getNumOperands() - 1; operandIdx >= 0;
+         operandIdx--) {
+      operands.push_back(currentOp->getOperand(operandIdx));
+    }
+    currentOp->erase();
+
+    for (auto operand : operands) {
+      Operation *definingOp = operand.getDefiningOp();
+      if (!definingOp) {
+        assert(isa<BlockArgument>(operand) && "Expected a block argument");
+        llvm::dbgs() << "Block argument. creating localget\n";
+        int localIndex = localIndexAnalysis.getLocalIndex(funcOp, operand);
+        builder.create<wasm::LocalGetOp>(newOp->getLoc(),
+                                         builder.getIndexAttr(localIndex));
+
+      } else if (isa<LocalOp>(definingOp)) {
+        definingOp->dump();
+        llvm::dbgs() << "Local operation. creating localget\n";
+        int localIndex =
+            localIndexAnalysis.getLocalIndex(funcOp, definingOp->getResult(0));
+        builder.create<wasm::LocalGetOp>(newOp->getLoc(),
+                                         builder.getIndexAttr(localIndex));
+      } else {
+        definingOp->dump();
+        llvm::dbgs() << "stackify success\n";
+        Operation *newOp = stackify(funcOp, index, ops, useCountAnalysis,
+                                    localIndexAnalysis, builder);
+        builder.setInsertionPoint(newOp);
+      }
+    }
+    return newOp;
+  }
+
+  Operation *stackifyOperation(Operation *op,
+                               LocalIndexAnalysis &localIndexAnalysis,
+                               OpBuilder &builder) {
+    Operation *newOp;
+    builder.setInsertionPoint(op);
+    if (isa<AddOp>(op)) {
+      TypeAttr typeAttr = TypeAttr::get(op->getResult(0).getType());
+      newOp = builder.create<wasm::AddOp>(op->getLoc(), typeAttr);
+    } else if (isa<ConstantOp>(op)) {
+      newOp = builder.create<wasm::ConstantOp>(op->getLoc(),
+                                               cast<ConstantOp>(op).getValue());
+    } else if (isa<LocalOp>(op)) {
+      FuncOp funcOp = op->getParentOfType<FuncOp>();
+      newOp = builder.create<wasm::LocalSetOp>(
+          op->getLoc(), builder.getIndexAttr(localIndexAnalysis.getLocalIndex(
+                            funcOp, op->getResult(0))));
+    } else if (isa<ReturnOp>(op)) {
+      newOp = builder.create<wasm::WasmReturnOp>(op->getLoc());
+    } else {
+      newOp = nullptr;
+    }
+    return newOp;
   }
 };
 } // namespace mlir::ssawasm
