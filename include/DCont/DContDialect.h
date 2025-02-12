3436a18f38f3c6802a20e843e3d65d7acd0b88a8
Byeongjee Kang <byeongjee.kang@gmail.com>
2025-02-11 22:27:15 -0500

    Add skeleton for delimited continuation dialect

---
+++ include/DCont/DContDialect.h
@@ -0,0 +1,18 @@
+//===- DContDialect.h - DCont dialect -----------------*- C++ -*-===//
+//
+// This file is licensed under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef DCONT_DCONTDIALECT_H
+#define DCONT_DCONTDIALECT_H
+
+#include "mlir/Bytecode/BytecodeOpInterface.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/IR/Dialect.h"
+
+#include "DCont/DContDialect.h.inc"
+
+#endif // DCONT_DCONTDIALECT_H
